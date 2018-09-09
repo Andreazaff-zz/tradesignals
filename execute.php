dbe6610de5bf5029498fe826179d6989a03e7280
Andrea Zaffignani <sicpoizn@gmail.com>
2018-09-09 14:49:14 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -1,53 +1,4 @@
 <?php
-
-	define("HOST", "localhost"); // E' il server a cui ti vuoi connettere.
-	define("USER", "u769934357_copy"); // E' l'utente con cui ti collegherai al DB.
-	define("PASSWORD", "xAm-Rg5-TFH-9vF"); // Password di accesso al DB.
-	define("DATABASE", "u769934357_user"); // Nome del database.
-	
-	$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
-	
-function set_config ($copy, $chat)
-{
-$url = "http://tradesignals.it/set_config.php?copy=".$copy."&chat=".$chat;
-    $ch = curl_init();
-    $optArray = array(
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true
-    );
-    curl_setopt_array($ch, $optArray);
-	$result = curl_exec($ch);
-	//echo $result;
-    curl_close($ch);	
-}
-
-function set_start ($boolean)
-{
-$url = "http://tradesignals.it/set_start.php?ts=".$boolean;
-    $ch = curl_init();
-    $optArray = array(
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true
-    );
-    curl_setopt_array($ch, $optArray);
-	$result = curl_exec($ch);
-	//echo $result;
-    curl_close($ch);	
-}
-
-function set_paid ($bool)
-{
-$url = "http://tradesignals.it/set_paid.php?bool=".$bool;
-    $ch = curl_init();
-    $optArray = array(
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true
-    );
-    curl_setopt_array($ch, $optArray);
-	$result = curl_exec($ch);
-	//echo $result;
-    curl_close($ch);	
-}
 	
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
@@ -70,58 +21,14 @@ $text = trim($text);
 $text = strtolower($text);
 
 $response = '';
-	if (strpos ($text, "/start") === 0 || $text=="ciao")
+	if (strpos ($text, "/start") === 0 )
 	{
-		$response = "Ciao $firstname, Benvenuto sul Configuratore del Copy di TradeSignals!\r\n\r\nHai a disposizione diversi comandi:\r\n/launch_copy per Attivare l'Investimento Automatico,\r\n/close_copy per Bloccarlo.\r\nNel caso in cui tu non sia ancora Abilitato al Servizio TradeSignals, inserisci /chatid e comunica il codice all'Admin @andreazaff che provvederà subito all'autenticazione!";
+		$response = $response = "Ciao $firstname, con questo Bot sarai in grado di ricevere le notifiche della MetaTrader 4 direttamente sul tuo account Telegram.\n\nIl codice di questa Chat è: $chatId\nInserisci questo codice nei parametri del tuo Expert Advisor e riceverai tutte le notifiche in questa chat.\n\nQuesto Bot è di proprietà di Andrea Zaffignani ed è compatibile solo con i suoi Expert Advisor.\nTutti i diritti sono riservati. ©\n";
 	}
-		else if ($text == "/chatid")
+		else
 		{
-			$response = "Il Tuo Codice Chat è: ".$chatId;
+			$response = "Comando Non Abilitato!\r\nContattare il Gestore del Servizio";
 		}
-			else if ($text == "/launch_copy")
-			{
-				set_config (1, $chatId);
-			$response = "L'operazione è andata a Buon Fine!\r\nHai Autorizzato TradeSignals ad aprire investimenti in automatico per tuo conto!";
-			}
-				else if ($text == "/close_copy")
-				{
-					set_config (0, $chatId);
-				$response = "L'operazione è andata a Buon Fine!\r\nHai Revocato TradeSignals ad aprire investimenti in automatico per tuo conto!";
-				}
-					else if (($text == "/ts_off") 
-								&& 
-								(
-									($chatId == "121096168") || ($chatId == "106805958")
-								)
-							)
-					{
-						set_start (0);
-						$response = "Gentile Admin, hai Spento l'Algoritmo di TradeSignals";
-					}
-						else if (($text == "/ts_on") 
-									&&
-									(
-										($chatId == "121096168") || ($chatId == "106805958")
-									)
-							)						
-						{
-							set_start (1);
-							$response = "Gentile Admin, hai Acceso l'Algoritmo di TradeSignals";
-						}
-							else if (($text == "/paid_off") && ($chatId == "121096168"))
-							{
-								set_paid (0);
-								$response = "Gentile Andrea, hai Chiuso il Copy per gli Abbonati";
-							}
-								else if (($text == "/paid_on") && ($chatId == "121096168"))
-								{
-									set_paid (1);
-									$response = "Gentile Andrea, hai Attivato il Copy per gli Abbonati";
-								}
-									else
-									{
-										$response = "Comando Non Abilitato!\r\nContattare il Gestore del Servizio";
-									}
 
 header("Content-Type: application/json");
 $parameters = array('chat_id' => $chatId, "text" => $response);
