99d66facddd6c05999be6564c768c4b70b507072
Andrea Zaffignani <sicpoizn@gmail.com>
2018-09-18 22:38:24 +0200

    Update

--- execute.php
+++ execute.php
@@ -1,18 +1,5 @@
 <?php
 
-function set_param ($id, $set)
-{
-	$url = "https://buddyzeus.com/zeusconfig_pageupdate_list01.php?id=".$id."&set=".$set;
-    $ch = curl_init();
-    $optArray = array(
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true
-    );
-    curl_setopt_array($ch, $optArray);
-	$result = curl_exec($ch);
-	//echo $result;
-    curl_close($ch);	
-}
 
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
@@ -36,25 +23,8 @@ $response = '';
 
 	if (strpos ($text, "/start") === 0 )
 	{
-		if ($chatId == -1001296319190)
-		{
-			$response = "Ciao $firstname, Benvenuto nel Pannello di Controllo Licenze di Zeus © v3.2\n\nIn questo gruppo saranno inviate tutte le licenze che verranno generate ed attivate nei vari client MT4.\n Utilizzando il comando \"Delete:(ID)\" dove il parametro (ID) costituisce il progressivo della licenza sarai in grado di modificare da remoto la licenza per la copia di Zeus © rispettivamente a quel Client.\nUtilizzando il comando \"Activate:(ID)\" sarai invece in grado di riattivarla.\n\nTutti i diritti sono riservati. ©\n";
-		}
-			else
-			{
-				$response = "Ciao $firstname, con questo Bot sarai in grado di ricevere le notifiche della MetaTrader 4 direttamente sul tuo account Telegram.\n\nIl codice di questa Chat è: $chatId\nInserisci questo codice nei parametri del tuo Expert Advisor e riceverai tutte le notifiche in questa chat.\n\nQuesto Bot è di proprietà di Andrea Zaffignani ed è compatibile solo con i suoi Expert Advisor.\nTutti i diritti sono riservati. ©\n";
-			}
+		$response = "Ciao! Sono il tuo bot personale";
 	}
-		else if ($string_exploded[0] == "delete" && $chatId == -1001296319190)
-		{	
-			set_param($string_exploded[1],0);
-			$response = "La Licenza Associata all'Account ID ".$string_exploded[1]." è stata Correttamente Disabilitata";
-		}
-			else if ($string_exploded[0] == "activate" && $chatId == -1001296319190)
-			{	
-				set_param($string_exploded[1],1);
-				$response = "La Licenza Associata all'Account ID ".$string_exploded[1]." è stata Correttamente Abilitata";
-			}
 				else
 				{
 					$response = "Comando Non Abilitato!\r\nContattare il Gestore del Servizio @andreazaff";
